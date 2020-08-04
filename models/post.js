054cb717b48ca9c2a848fb5f4298b96be2927d1c
Jeremiah <jscott4720@gmail.com>
2020-08-04 15:15:23 -0400

    added post model

---
+++ models/post.js
@@ -0,0 +1,11 @@
+const mongoose = require('../db/connection')
+const Schema = mongoose.Schema
+
+const postSchema = new Schema ({
+    post: String,
+    comments: [String]
+})
+
+Post = mongoose.model("Post", postSchema)
+
+module.exports = Post
